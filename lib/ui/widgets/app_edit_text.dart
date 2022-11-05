b20c1c33c466869ad091a9262abbf6e3cd2bcaa9
waheduzzaman <me@waheduzzaman.com>
2022-11-05 23:39:34 +0600

    fixed edit text padding issue

--- lib/ui/widgets/app_edit_text.dart
+++ lib/ui/widgets/app_edit_text.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:GID/core/ui_constants.dart';
 import 'package:flutter/material.dart';
 
 import 'package:GID/core/ui_color_constant.dart';
@@ -19,36 +20,44 @@ class AppEditText extends StatefulWidget {
 }
 
 class _AppEditTextState extends State<AppEditText> {
-  bool showPassword = false;
+  bool hidePassword = true;
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: paddingH12,
       width: double.infinity,
-      height: 50,
+      height: 70,
       decoration: BoxDecoration(
         color: getBackgroundColor(),
         borderRadius: BorderRadius.circular(12),
       ),
-      child: TextFormField(
-        obscureText: (widget.isObscure && showPassword),
-        maxLines: 1,
-        decoration: InputDecoration(
-            suffixIcon: widget.isObscure
-                ? (IconButton(
-                    onPressed: () {
-                      setState(() {
-                        showPassword = !showPassword;
-                      });
-                    },
-                    icon: Icon(
-                        showPassword ? Icons.visibility : Icons.visibility_off),
-                  ))
-                : null,
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 4, horizontal: 24),
-            enabledBorder: InputBorder.none,
-            focusedBorder: InputBorder.none,
-            hintText: "${widget.hint}"),
+      child: Center(
+        child: TextFormField(
+          obscureText: (widget.isObscure && hidePassword),
+          maxLines: 1,
+          decoration: InputDecoration(
+              suffixIcon: widget.isObscure
+                  ? Container(
+                      child: (GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            hidePassword = !hidePassword;
+                          });
+                        },
+                        child: Icon(hidePassword
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                      )),
+                    )
+                  : null,
+              suffixIconConstraints: const BoxConstraints(
+                minWidth: 2,
+                minHeight: 2,
+              ),
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              hintText: "${widget.hint}"),
+        ),
       ),
     );
   }
